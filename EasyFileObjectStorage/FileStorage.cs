ef3dcefd8d62da2b5a5fb14874374fb2c8ded852
Anton Bessolitsyn <antonbessolitsyn@yandex.ru>
2025-04-08 17:13:59 +0300

    Add easyObjectFileStorage

---
+++ EasyFileObjectStorage/FileStorage.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System.Globalization;
+using System.Xml;
+
+namespace EasyObjectFileStorage
+{
+    public abstract class FileStorage(string pathToRootFolder)
+    {
+        readonly string RootFolder = pathToRootFolder;
+
+        public event EventHandler<string>? LogEvent;
+
+        protected void SaveFile(string path, string filename, string fileContent, out string fileid)
+        {
+            fileid = path + filename;
+            System.IO.File.WriteAllText(@RootFolder + path + filename, fileContent);
+        }
+
+        protected void SaveFile(string path, string filename, byte[] fileContent, out string fileid)
+        {
+            fileid = path + filename;
+            System.IO.File.WriteAllBytes(@RootFolder + path + filename, fileContent);
+        }
+
+        protected void Logging(string message)
+        {
+            LogEvent?.Invoke(this, message);
+        }
+
+        protected string[] GetAllFiles(string path, string fileExt)
+        {
+            string[] files = Directory.GetFiles(@RootFolder + path, fileExt);
+            return files;
+        }
+
+        
+
+
+
+
+    }
+}
---
+++ EasyFileObjectStorage/JsonFileStorage.cs
@@ -0,0 +1,125 @@
+﻿using EasyDataCollector;
+using EasyDataCollector.Datasets;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyObjectFileStorage
+{
+    public class JsonFileStorage : FileStorage 
+    {
+        const string EX_MESSAGE_WHEN_SAVING = $"FileStorageMessage_ Error when saving;";
+        const string EX_MESSAGE_WHEN_READING = $"FileStorageMessage_ Error when reading;";
+        readonly string FOLDER_FOR_DATASET_STORAGE;
+        readonly string FILE_EXTENSION;
+
+
+        public JsonFileStorage(string pathToRootFolder, string subfolder, string fileExtension = "*.json") : base(pathToRootFolder)
+        {
+            FOLDER_FOR_DATASET_STORAGE = subfolder;
+            FILE_EXTENSION = fileExtension;
+            
+        }
+        
+        public object[] GetAllObjects()
+        {
+            try
+            {
+                string[] files = GetAllFiles($"/{FOLDER_FOR_DATASET_STORAGE}/", FILE_EXTENSION);
+                var contents = GetAllDeserializeObject<object>(files);
+                return contents;
+            }
+            catch (Exception ex)
+            {
+                string logmessage = $"{EX_MESSAGE_WHEN_READING} ex.message; {ex.Message}";
+                Logging(logmessage);
+                //TO DO Logging must be done!:) 
+                throw;
+            }
+        }
+
+        public T[] GetAllObject<T>()
+        {
+            try
+            {
+                string[] files = GetAllFiles($"/{FOLDER_FOR_DATASET_STORAGE}/", FILE_EXTENSION);
+                var contents = GetAllDeserializeObject<T>(files);
+                return contents;
+            }
+            catch (Exception ex)
+            {
+                string logmessage = $"{EX_MESSAGE_WHEN_READING} ex.message; {ex.Message}";
+                Logging(logmessage);
+                //TO DO Logging must be done!:) 
+                throw;
+            }
+        }
+
+        public void SaveObject(object obj)
+        {
+            try
+            {
+                var fileContent = JsonConvert.SerializeObject(obj, Formatting.Indented);
+                var filename = obj.GetHashCode().ToString(CultureInfo.InvariantCulture) + FILE_EXTENSION;
+                SaveFile($"/{FOLDER_FOR_DATASET_STORAGE}/", filename, fileContent, out var _);
+            }
+            catch (Exception ex)
+            {
+                string logmessage = $"{EX_MESSAGE_WHEN_SAVING} ex.message; {ex.Message}";
+                Logging(logmessage);
+                //TO DO Logging must be done!:) 
+                throw;
+            }
+        }
+
+        public void SaveNewDataset(object? obj)
+        {
+            try
+            {
+                var fileContent = JsonConvert.SerializeObject(obj, Formatting.Indented);
+                var filename = obj.GetHashCode().ToString(CultureInfo.InvariantCulture) + FILE_EXTENSION;
+                SaveFile($"/{FOLDER_FOR_DATASET_STORAGE}/", filename, fileContent, out var _);
+
+            }
+            catch (Exception ex)
+            {
+                string logmessage = $"{EX_MESSAGE_WHEN_SAVING} ex.message; {ex.Message}";
+                //TO DO Logging must be done!:) 
+                Logging(logmessage);
+                throw;
+            }
+        }
+
+        public void SaveObjectStreamToFile(MemoryStream sourceStream, string filename, out string fileid)
+        {
+            try
+            {
+                SaveFile($"/{FOLDER_FOR_DATASET_STORAGE}/", filename, sourceStream.ToArray(), out fileid);
+            }
+            catch (Exception ex)
+            {
+                string logmessage = $"{EX_MESSAGE_WHEN_SAVING} ex.message; {ex.Message}";
+                //TO DO Logging must be done!:) 
+                Logging(logmessage);
+                throw;
+            }
+        }
+
+        static T[] GetAllDeserializeObject<T>(string[] files)
+        {
+            T[] contents = new T[files.Length];
+            for (int i = 0; i < files.Length; i++)
+            {
+
+                var content = JsonConvert.DeserializeObject<T>(System.IO.File.ReadAllText(files[i]));
+                if (content != null)
+                    contents[i] = content;
+            }
+            return contents;
+        }
+    }
+}
