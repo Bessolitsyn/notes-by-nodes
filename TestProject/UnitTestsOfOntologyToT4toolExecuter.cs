35ba8736d6aa8e7a14b0341de2d42451c7cabab6
Anton Bessolitsyn <antonbessolitsyn@yandex.ru>
2025-04-10 16:39:18 +0300

    Tests: extend OwlToT4Tests and add sandbox class

--- TestProject/UnitTestsOfOntologyToT4toolExecuter.cs
+++ TestProject/UnitTestsOfOntologyToT4toolExecuter.cs
@@ -6,12 +6,48 @@ namespace TestProject
         [Fact]
         public void ReadOntologyForOneClass()
         {
-            string ontoDirectory1 = "c:\\Users\\tocha\\source\\notes-by-nodes\\notes-by-nodes\\Entities\\notes-by-nodes.rdf";
-            string templatesDirectory1 = "c:\\Users\\tocha\\source\\notes-by-nodes\\TestProject\\Ontology";
-            string nameSpace1 = "OWLtoT4templates.Ontology";
-            OntologyToT4toolExecuter.ReadOntologyForOneClass("http://notes-by-nodes/ontologies/2025/Node", ontoDirectory1, templatesDirectory1, nameSpace1);
+            string ontoDirectory1 = "c:\\Users\\tocha\\source\\notes-by-nodes\\notes-by-nodes\\Ontology\\notes-by-nodes.rdf";
+            OntologyToT4toolExecuter.TemplatesDirectory = "c:\\Users\\tocha\\source\\notes-by-nodes\\TestProject\\Ontology";
+            OntologyToT4toolExecuter.TemplatesDirectoryForRecords = "c:\\Users\\tocha\\source\\notes-by-nodes\\TestProject\\Ontology";
+            OntologyToT4toolExecuter.NameSpace = "OWLtoT4templates.TOntology";
+            OntologyToT4toolExecuter.NameSpaceForRecords = "OWLtoT4templates.TRecords";
+            OntologyToT4toolExecuter.DeleteFiles(OntologyToT4toolExecuter.TemplatesDirectory);
+
+            OntologyToT4toolExecuter.ReadOntologyForOneClass("http://notes-by-nodes/ontologies/2025/Node", ontoDirectory1, false);
+
+            Assert.True(Directory.GetFiles(OntologyToT4toolExecuter.TemplatesDirectory).Length == 3);
+            
+        }
+
+        [Fact]
+        public void ReadOntologyTestRecordWithParentRecord()
+        {
+            string ontoDirectory1 = "c:\\Users\\tocha\\source\\notes-by-nodes\\notes-by-nodes\\Ontology\\notes-by-nodes.rdf";
+            OntologyToT4toolExecuter.TemplatesDirectory = "c:\\Users\\tocha\\source\\notes-by-nodes\\TestProject\\Ontology";
+            OntologyToT4toolExecuter.TemplatesDirectoryForRecords = "c:\\Users\\tocha\\source\\notes-by-nodes\\TestProject\\Ontology";
+            OntologyToT4toolExecuter.NameSpace = "OWLtoT4templates.TOntology";
+            OntologyToT4toolExecuter.NameSpaceForRecords = "OWLtoT4templates.TRecords";
+            OntologyToT4toolExecuter.DeleteFiles(OntologyToT4toolExecuter.TemplatesDirectory);
+
+            OntologyToT4toolExecuter.ReadOntologyForOneClass("http://notes-by-nodes/ontologies/2025/Node", ontoDirectory1, true);
+            OntologyToT4toolExecuter.ReadOntologyForOneClass("http://notes-by-nodes/ontologies/2025/Note", ontoDirectory1, true);
+
+            Assert.True(Directory.GetFiles(OntologyToT4toolExecuter.TemplatesDirectory).Length == 10);
+
+        }
+
+        [Fact]
+        public void ReadOntology()
+        {
+            string ontoDirectory1 = "c:\\Users\\tocha\\source\\notes-by-nodes\\notes-by-nodes\\Ontology\\notes-by-nodes.rdf";
+            OntologyToT4toolExecuter.TemplatesDirectory = "c:\\Users\\tocha\\source\\notes-by-nodes\\TestProject\\Ontology";
+            OntologyToT4toolExecuter.TemplatesDirectoryForRecords = "c:\\Users\\tocha\\source\\notes-by-nodes\\TestProject\\Ontology";
+            OntologyToT4toolExecuter.NameSpace = "OWLtoT4templates.TOntology";
+            OntologyToT4toolExecuter.NameSpaceForRecords = "OWLtoT4templates.TRecords";
+            //string ontoDirectory1 = "c:\\Users\\tocha\\source\\notes-by-nodes\\notes-by-nodes\\Ontology\\notes-by-nodes.rdf";
+            OntologyToT4toolExecuter.ReadOntology(ontoDirectory1, true);
             Assert.True(true);
-            //OntologyToT4toolExecuter.DeleteFiles(templatesDirectory1);           
+
         }
     }
 }
