032e76b1c65423d01584ef0226b77b6d54c0bf2b
Anton Bessolitsyn <antonbessolitsyn@yandex.ru>
2025-04-15 10:29:56 +0300

    finished app rules level test

--- TestProject/AppRulesLevelTests.cs
+++ TestProject/AppRulesLevelTests.cs
@@ -11,6 +11,19 @@ namespace TestProject
 {
     public class AppRulesLevelTests
     {
+        [Fact]
+        public static void TestUserBoxNodes()
+        {
+            var storageFactory = new NodeStorageTestFactory();
+            LocalUser user = new("Anton", "Anton@mailbox", storageFactory.GetUserStorage());
+            LocalBox box = new(user, storageFactory.GetBoxStorage(), "Name of the test box", "Description of the test box");
+            
+            bool boxWasAddedToUser = user.HasChildNodes.Count()==1 && box.HasParentNode==user;
+            box.RemoveFromChildNodes(box);
+            bool boxWasDeletedFromUser = user.HasChildNodes.Count()==0;
+            Assert.True(boxWasDeletedFromUser && boxWasDeletedFromUser);
+
+        }
 
         [Fact]
         public static void TestUserConstructor()
@@ -31,13 +44,18 @@ namespace TestProject
         }
 
         [Fact]
-        public static void TestNodeConstructor()
+        public static void TestLocalNoteConstructor()
         {
             var storageFactory = new NodeStorageTestFactory();
             LocalUser user = new("Anton", "Anton@mailbox", storageFactory.GetUserStorage());
             LocalBox box = new(user, storageFactory.GetBoxStorage(), "Name of the test box", "Description of the test box");
             LocalNote note = new LocalNote(box, "Untitled 1", "Description of UntitledNote1");
-            Assert.True(box.HasChildNodes.Count() == 1);
+            LocalNote note2 = new LocalNote(note, "Untitled 2", "Description of UntitledNote2");
+            Assert.True(box.HasChildNodes.Contains(note) &&
+                note.HasParentNode == box &&
+                note.HasChildNodes.Contains(note2) &&
+                note2.HasParentNode == note
+                );
         }
     }
 
