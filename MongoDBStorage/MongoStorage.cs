eefd2057c8225607e710882cc3ea607ab170967a
Anton Bessolitsyn <antonbessolitsyn@yandex.ru>
2025-06-30 14:43:29 +0300

    Новый модуль - mongodb

---
+++ MongoDBStorage/MongoStorage.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+
+namespace MongoDBStorage
+{
+    public class MongoStorage
+    {
+        MongoClient _client;
+        IMongoDatabase _mongoDatabase;
+        MongoStorage()
+        {            
+            _client = InitClient();
+        }
+        public static MongoClient InitClient()
+        {
+            var connectionString = "mongodb://localhost:27017";
+            return new MongoClient(connectionString);
+        }
+    }
+}
