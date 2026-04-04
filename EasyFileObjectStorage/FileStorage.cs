6c2e239320cf6002d670d627a1d933fa3979594c
Anton Bessolitsyn <antonbessolitsyn@yandex.ru>
2026-04-04 18:40:18 +0300

    refactoring

--- EasyFileObjectStorage/FileStorage.cs
+++ EasyFileObjectStorage/FileStorage.cs
@@ -6,44 +6,56 @@ using System.Xml;
 
 namespace EasyObjectFileStorage
 {
-    public abstract class FileStorage(string pathToRootFolder)
+    public abstract class FileStorage
     {
         private static readonly ConcurrentDictionary<string, SemaphoreSlim> _fileLockers = [];
-        readonly string _rootFolder = pathToRootFolder;
+        readonly DirectoryInfo _rootFolder;
 
         public event EventHandler<string>? LogEvent;
+        public FileStorage(string pathToRootFolder)
+        {
+            _rootFolder = new DirectoryInfo(pathToRootFolder.TrimEnd());
+            if (!_rootFolder.Exists) _rootFolder.Create();
+        }
 
         protected async Task<string> SaveFileAsync(string path, string filename, string fileContent)
         {
-            var fileid = path + filename;
-            var filePath = _rootFolder + path + filename;
-            await SafeFileActionAsync(filePath, async () => await System.IO.File.WriteAllTextAsync(filePath, fileContent));
+            async Task WriteAllTextAsync(FileInfo file, string content)
+            {
+                using var s = file.CreateText();
+                await s.WriteAsync(content);
+                s.Close();
+            }
+            var fileid = Path.Combine(path, filename);
+            var filePath = new FileInfo(Path.Combine(_rootFolder.FullName, fileid));
+
+            await SafeFileActionAsync(filePath.FullName, async () => await WriteAllTextAsync(filePath, fileContent));
             return fileid;
 
         }
-        protected void SaveFile(string path, string filename, string fileContent, out string fileid)
-        {
-            fileid = path + filename;
-            System.IO.File.WriteAllText(_rootFolder + path + filename, fileContent);
-        }       
-        protected void SaveFile(string path, string filename, byte[] fileContent, out string fileid)
-        {
-            fileid = path + filename;
-            System.IO.File.WriteAllBytes(_rootFolder + path + filename, fileContent);
-        }
+
         protected async Task<string> SaveFileAsync(string path, string filename, byte[] fileContent)
         {
-            var fileid = path + filename;
-            var filePath = _rootFolder + path + filename;
-            await SafeFileActionAsync(filePath, async () =>  await System.IO.File.WriteAllBytesAsync(filePath, fileContent));
+            async Task WriteAllBytesAsync(FileInfo file, byte[] content)
+            {
+                using var fs = file.Create();
+                fs.Position = 0;
+                await fs.WriteAsync(content, 0, content.Length);
+                fs.Close();
+
+            }
+            var fileid = Path.Combine(path, filename);
+            var filePath = new FileInfo(Path.Combine(_rootFolder.FullName, fileid));
+            await SafeFileActionAsync(filePath.FullName, async () =>  await WriteAllBytesAsync(filePath, fileContent));
             return fileid;
 
         }
         protected void RemoveFile(string path, string filename)
         {
-            var filePath = _rootFolder + path + filename;
-            var _ = SafeFileActionAsync(filePath, () => Task.Run(() => File.Delete(filePath)));
-           
+            var fileid = Path.Combine(path, filename);
+            var filePath = new FileInfo(Path.Combine(_rootFolder.FullName, fileid));
+
+            var _ = SafeFileActionAsync(filePath.FullName, () => Task.Run(() => filePath.Delete()));           
         }
 
         protected void Logging(string message)
@@ -53,7 +65,7 @@ namespace EasyObjectFileStorage
 
         protected FileInfo[] GetAllFiles(string path, string fileExt)
         {
-            var dirInfo = new DirectoryInfo(Path.Join(_rootFolder.TrimEnd(), path.TrimEnd()));
+            var dirInfo = new DirectoryInfo(Path.Join(_rootFolder.FullName, path.TrimEnd()));
             var  files = dirInfo.GetFiles($"*.{fileExt}");//Directory.GetFiles(_rootFolder + path, $"*.{fileExt}");
             return files;
         }
